3c168a29f61092283aa912f2de4869c125eff65b
abj1210 <87817992+abj1210@users.noreply.github.com>
2024-06-19 18:54:44 +0800

    v3.6 test

--- test/testsmall.cpp
+++ test/testsmall.cpp
@@ -16,12 +16,12 @@ TEST(Testsmall, tests2) {
           "ababrteyuioiuetwreyuioyjfhtdfdfsdfsdfdsfgfhtfhtfhfyhfthdrhdgsrfseaba"
           "brteyuioiuetwreyuioyjfhtdfdfsdfsdfdsfgfhtfhtfhfyhfthdrhdgsrf"));
 }
-/*
+
 TEST(Testsmall, roundtest) {
   srand(time(NULL));
   char buf[257];
   buf[256] = '\0';
-  for (int i = 0; i < 256; i++) {
+  for (int i = 0; i < 64; i++) {
     printf("Test num : %d\n", i);
     for(int j = 0; j < 256; j++) {
       buf[j] = rand();
@@ -29,7 +29,7 @@ TEST(Testsmall, roundtest) {
     EXPECT_EQ(1, makeFullTest(buf));
   }
 }
-*/
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
