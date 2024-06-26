f4f7eec45ececd7355c8ea6cf79d270b593bbb84
abj1210 <87817992+abj1210@users.noreply.github.com>
2024-06-27 00:28:58 +0800

    Update testshash.cpp

--- test/testshash.cpp
+++ test/testshash.cpp
@@ -5,20 +5,21 @@ char str[] =
     "ababrteyuioiuetwreyuioyjfhtdfdfsdfsdfdsfgfhtfhtfhfyhfthdrhdgsrfseaba"
     "brteyuioiuetwreyuioyjfhtdfdfsdfsdfdsfgfhtfhtfhfyhfthdrhdgsrfse";
 
-TEST(Testsmode, testsha1) { 
-  for(int i = 0; i < 5 ; i++) 
-    EXPECT_EQ(1, makeFullTest(str, 0x00+i));
+TEST(Testsmode, testsha1)
+{
+  EXPECT_EQ(1, makeFullTest(str, 0x00));
 }
-TEST(Testsmode, testmd5) { 
-  for(int i = 0; i < 5 ; i++) 
-    EXPECT_EQ(1, makeFullTest(str, 0x10+i));
+TEST(Testsmode, testmd5)
+{
+  EXPECT_EQ(1, makeFullTest(str, 0x10));
 }
-TEST(Testsmode, testsha256) { 
-  for(int i = 0; i < 5 ; i++) 
-    EXPECT_EQ(1, makeFullTest(str, 0x20+i));
+TEST(Testsmode, testsha256)
+{
+  EXPECT_EQ(1, makeFullTest(str, 0x20));
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
